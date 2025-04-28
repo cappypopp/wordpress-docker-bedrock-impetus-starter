5ad0e341ba47c581c6a933e6b91aea51faf1122e
Cappy Popp <cappy@thoughtlabs.com>
2025-04-28 11:45:23 -0400

    Added Bedrock and Sage commented out in setup.zsh
    WHY: I already ran them, the files they create are already in the repo.

--- setup.zsh
+++ setup.zsh
@@ -3,6 +3,10 @@
 # Exit immediately on error
 set -e
 
+# Install Bedrock
+# Since I already did this, I'm commenting it out
+# composer create-project roots/bedrock .
+
 # Copy and configure environment
 cp -n .env.example .env || echo ".env already exists"
 
@@ -17,6 +21,11 @@ sed -i '' 's/database_user/wordpress/' .env
 sed -i '' 's/database_password/secret/' .env
 sed -i '' 's/username_here/admin/' .env
 sed -i '' 's/password_here/asdf3456POI/' .env
+
+# Move into themes and install Sage
+# cd web/app/themes
+# composer create-project roots/sage your-sage-theme
+# cd ../../..
 
 echo "🔒 YOU MUST Generate your keys here: https://roots.io/salts.html and put them in the .env file!"
 
