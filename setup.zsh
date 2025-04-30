e4081aa538524c912553654caebc5ee1211f78f8
Cappy Popp <cappy@thoughtlabs.com>
2025-04-30 11:59:55 -0400

    FIX: remove redundant sed commands to set .env

--- setup.zsh
+++ setup.zsh
@@ -10,18 +10,6 @@ set -e
 # Copy and configure environment
 cp -n .env.example .env || echo ".env already exists"
 
-# Sets default database credentials:
-# Database name: wordpress
-# Database user: wordpress
-# Database password: secret
-# Username: admin
-# Password: asdf3456POI
-sed -i '' 's/database_name/wordpress/' .env
-sed -i '' 's/database_user/wordpress/' .env
-sed -i '' 's/database_password/secret/' .env
-sed -i '' 's/username_here/admin/' .env
-sed -i '' 's/password_here/asdf3456POI/' .env
-
 # Move into themes and install Sage
 # cd web/app/themes
 # composer create-project roots/sage your-sage-theme
