44124d39bad11dae0fe35b27368922c17f44edcb
Cappy Popp <cappy@thoughtlabs.com>
2025-04-30 11:46:44 -0400

    Rewrote to use mkcert and fresh-start make target

--- scripts/setup.zsh
+++ scripts/setup.zsh
@@ -12,30 +12,13 @@ MAGENTA="\033[1;35m"
 NC="\033[0m" # No Color
 
 echo ""
-echo "${BLUE}📦 Installing PHP and Node dependencies via Composer...${NC}"
-make install
-
-echo ""
 echo "${BLUE}🐳 Building and 🚀 Restarting Docker images...${NC}"
 make full-rebuild
-
-echo ""
-echo "${BLUE}🎨 Building Sage assets (Vite, Tailwind)...${NC}"
-make dev
-
-echo ""
-echo "${BLUE}📦 Set Up WordPress${NC}"
-make initial-wp-setup
-
-echo ""
-echo "${YELLOW}🔒 Reminder: Check on WordPress admin password and reset using /scripts/wp-admin-pw-reset.zsh${NC}"
-echo "You MUST set your password in .env before running this script!"
-make reset-password
 
 echo ""
 echo "${BLUE}🔒 Create SSL certificates and add them to the project${NC}"
 make ssl-cert
 
 echo ""
-echo "${MAGENTA}🎉 Setup complete! Visit: ${GREEN}https://localhost:8443${NC}"
+echo "${MAGENTA}🎉 Setup complete! Visit: ${GREEN}https://localhost:${WP_HTTPS_PORT}${NC}"
 echo ""
