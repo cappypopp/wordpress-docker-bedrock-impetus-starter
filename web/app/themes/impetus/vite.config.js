6efdef2b27f9e1bba3575d6612453e785a592429
Cappy Popp <cappy@thoughtlabs.com>
2025-04-30 14:58:33 -0400

    FIX: Complete Redo to Get Tailwind v4 Working

--- web/app/themes/impetus/vite.config.js
+++ web/app/themes/impetus/vite.config.js
@@ -1,38 +1,43 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite';
-import laravel from 'laravel-vite-plugin'
-import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
+import { wordpressPlugin, wordpressThemeJson } from "@roots/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  base: '/app/themes/sage/public/build/',
-  plugins: [
-    tailwindcss(),
-    laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-        'resources/css/editor.css',
-        'resources/js/editor.js',
-      ],
-      refresh: true,
-    }),
+export default defineConfig(({ mode }) => {
+  // Load env file based on mode (development/production)
+  const env = loadEnv(mode, "../../../", ""); // going up to project root where .env lives
 
-    wordpressPlugin(),
+  return {
+    base: `/app/themes/${env.WP_THEME_NAME}/public/build/`,
+    plugins: [
+      laravel({
+        input: [
+          "resources/css/app.css",
+          "resources/js/app.js",
+          "resources/css/editor.css",
+          "resources/js/editor.js",
+        ],
+        refresh: true,
+      }),
 
-    // Generate the theme.json file in the public/build/assets directory
-    // based on the Tailwind config and the theme.json file from base theme folder
-    wordpressThemeJson({
-      disableTailwindColors: false,
-      disableTailwindFonts: false,
-      disableTailwindFontSizes: false,
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@scripts': '/resources/js',
-      '@styles': '/resources/css',
-      '@fonts': '/resources/fonts',
-      '@images': '/resources/images',
+      wordpressPlugin(),
+
+      // Generate the theme.json file in the public/build/assets directory
+      // based on the Tailwind config and the theme.json file from base theme folder
+      wordpressThemeJson({
+        disableTailwindColors: false,
+        disableTailwindFonts: false,
+        disableTailwindFontSizes: false,
+      }),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        "@scripts": "/resources/js",
+        "@styles": "/resources/css",
+        "@fonts": "/resources/fonts",
+        "@images": "/resources/images",
+      },
     },
-  },
-})
+  };
+});
